32d6e2908c2093ffefc5e9dfb04848255a502712
husenshai123 <husenshaikh482005@gmail.com>
2026-06-20 12:32:42 +0530

    update backend url

--- src/components/Guestbook.jsx
+++ src/components/Guestbook.jsx
@@ -19,7 +19,7 @@ export default function Guestbook() {
     if (!name || !message) return;
 
     try {
-      const response = await fetch('http://localhost:5000/api/guestbook', {
+      const response = await fetch('https://sunilsurywanshi-backend.vercel.app/api/guestbook', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
